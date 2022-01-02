ff0cbd98f2f8c3be68a2b69e99d7921913db67f4
HurtowniaKechupu <>
2022-01-02 18:39:03 +0100

    Począteczek

--- Main.py
+++ Main.py
@@ -1,2 +1,34 @@
-print('works')
+import Standard_Queue
 
+# Parametry początkowe symulacji
+MAX_CZAS_SYMULACJI = 10000
+lam = -1
+while lam not in [1, 2, 3]:
+    lam = int(input("\nProszę wprowadzić wartość lambda wybierając z {1,2,3}: "))
+    if lam in [1, 2, 3]:
+        print("lambda = " + str(lam))
+    else:
+        print("Wybrano nieprawidłową wartość lambda. Proszę spróbować ponownie.\n")
+mi = 4
+ro = lam / mi
+acs = 0.0
+obsluzonych_zdarzen = 0  # liczba obsluzonych zdarzen (Real)
+czas_obslugi_imag = 0  # czas obslugi klientow IMAG (do prawdopodobienstwa)
+czas_obslugi_real = 0  # czas obslugi klientow REAL (do klientow w systemie)
+zdarzen_w_kolejce = 0
+czasy_przyjscia = []
+czasy_rozpoczecia = []
+odst_mdz_zgl = 0
+czas_p_zero = 0
+
+# Uruchomienie symulacji
+mode = -1
+while mode not in [1, 2]:
+    mode = int(input("\nWybierz rodzaj kolejki do symulacji, wpisując odpowiednią cyfrę, gdzie:\n"
+                        "   1) Standardowa kolejka M/M/1  2) Kolejka M/M/1 Continuous Service: "))
+    if mode == 1:
+        wynik_standard = standard.uruchom_MM1()
+    elif mode == 2:
+        wynik_continuous = continuous_service.uruchom_MM1CS()
+    else:
+        print("Wprowadzono nieprawidłowy numer wyboru kolejki. Proszę spróbować ponownie.\n")
---
+++ Methods.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+def gen_t_obslugi(self):
+    return -np.log(1 - np.random.random()) / self.mi
+
+
+def gen_t_przyjscia(self):
+    return -np.log(1 - np.random.random()) / self.lam
+
+
+def obl_sr_licz_kl_w_buf(self):
+    suma = 0
+    for i in range(len(self.ile_zdarzen) - 1):
+        suma += ((self.lista_czasow[i + 1] - self.lista_czasow[i]) * self.ile_zdarzen[i])
+    return suma / self.acs
+
+
+def obl_sr_licz_kl_w_sys(self):
+    wynik = self.obl_sr_licz_kl_w_buf() + self.czas_obslugi_real / self.acs
+    return wynik
+
+
+def obl_sr_czas_ocz_na_obs(self):
+    suma = 0
+    for i in range(self.obsluzonych_zdarzen):
+        suma += (self.czasy_rozpoczecia[i] - self.czasy_przyjscia[i])
+    return suma / self.obsluzonych_zdarzen
+
+
+def obl_sr_czas_przej_przez_sys(self):
+    suma = 0
+    for i in range(self.obsluzonych_zdarzen):
+        suma += (self.czasy_rozpoczecia[i] - self.czasy_przyjscia[i] + 1 / self.mi)
+    return suma / self.obsluzonych_zdarzen
