d7714d747374190683d3275953acdb3e74eeddca
HurtowniaKechupu <>
2022-01-05 18:17:41 +0100

    chyba działa

--- Main.py
+++ Main.py
@@ -1,21 +1,19 @@
 from Ustawienia import *
 from Symulacja import *
-from Statistics import *
 
 #todo spolszczyć
 #todo dodać drukowanie do pliku
 
-#todo naprawić tą funkcję bo nie działa
-def print_states(x, y, z):
-    print("Zmiany stanu systemu (self.obecnyCzasSystemu, self.stan): {}\n"
-          "Zdarzenia w systemie (self.obecnyCzasSystemu, self.systemEvent is None ? 0 : 1): {}\n"
-          "Zdarzenia w kolejce (self.obecnyCzasSystemu, self.kolejkaZdarzen.size()): {}"
-          .format(x, y, z))
+def dopliku(tempLambda, mean):
+    plik = open('wyniki.txt', 'a')
+    plik.write(f'{tempLambda} ; {mean} \n')
+    plik.close()
 
 
 def main():
     zadanie = 1
     seed = 128
+    open('wyniki.txt', 'w').close() # czyszczenie pliku z wynikami
 
     if zadanie == 1:
         minLambda = 0.5
@@ -46,8 +44,6 @@ def main():
 
     print("Symulator kolejki M/M/1 - Zadanie", zadanie)
     np.random.seed(ustawienia.seed)
-    #todo tutaj statistics
-    statistics = Statistics(ustawienia)
     tempLambda = ustawienia.minLambda
 
     while tempLambda <= ustawienia.maxLambda:  # symulacje dla wielu wartości lambda
@@ -65,12 +61,10 @@ def main():
             meanDelaySystemTimeSum += meanDelaySim
             np.random.seed(ustawienia.seed + 2137 * i)  # zmiana ziarna po każdej symulacji
             print("Symulacja nr: {}, E[T] w aktualnej symulacji: {}\n".format(i + 1, meanDelaySim))
-            #print_states(symulacja.system.systemState, symulacja.system.systemEvents, symulacja.system.queueEvents)
-            print("------------------------------------------------------------------------------------------------------------------------------------\n")
+            print("_________________________________________________________________________________________________________\n")
         print("E[T] dla danej wartości Lambda:", meanDelaySystemTimeSum / ustawienia.liczbaSymulacji)
-        statistics.addStatistics(meanDelaySystemTimeSum / ustawienia.liczbaSymulacji, tempLambda)
+        dopliku(tempLambda, meanDelaySystemTimeSum / ustawienia.liczbaSymulacji)
         tempLambda += 0.25
 
-    statistics.plot()
 
 main()
